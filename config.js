95578db7e3f36bcf1faf0877f1a8e435892fee04
Jintarq <71939344+Jintarq@users.noreply.github.com>
2021-04-07 19:13:04 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  consumer_key: "vOnRErKRpKqcem29LNgbS3m2u",
-  consumer_secret: "364teLk1oRskFOKZSA5SgGevqNgLRjwYXmEH5kLECuL7xest6j",
-  access_token_key: "1377245978714071042-2F4kZ0xkuz5qSvLysGSFkAYpzjNai9",
-  access_token_secret: "cNNKDs4s3SpH40b59heHCcTMfjRc6gvi6Kw3PxJDggZ2F",
+  consumer_key: "",
+  consumer_secret: "",
+  access_token_key: "",
+  access_token_secret: "",
 };
