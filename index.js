2a62f80c8a8b6e7a6e2e8c3a5d384e0ac94229a5
Jintarq <71939344+Jintarq@users.noreply.github.com>
2021-04-08 07:27:57 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const config = require("./config");
 const twitter = require("twitter-lite");
 const client = new twitter(config);
 const fetch = require("node-fetch");
+const schedule = require("node-schedule");
 
-setTimeout(() => {
+const job = schedule.scheduleJob({ hour: 20, minute: 25 }, () => {
   fetch("https://coronavirusapi-france.now.sh/FranceLiveGlobalData")
     .then((response) => {
       return response.json();
@@ -12,11 +13,11 @@ setTimeout(() => {
       const datas = responseData.FranceGlobalLiveData[0];
       client
         .post("statuses/update", {
-          status: `Information(s) COVID-19 en ${datas.nom}.\r\nLe : ${datas.date}.\r\nDonnées :\r\nDécès totaux en hôpital : ${datas.deces}.\r\nHospitalisations totales : ${datas.hospitalises}. (Nouvelles hospitalisations ce jour -> ${datas.nouvellesHospitalisations})\r\nRéanimations totales : ${datas.reanimation} (Nouvelles réanimations ce jour -> ${datas.nouvellesReanimations}).\r\nSource => ${datas.source.nom}. `,
+          status: `Information(s) COVID-19 en ${datas.nom}.\r\nDate : ${datas.date}.\r\nDonnées :\r\nDécès totaux en hôpital : ${datas.deces}.\r\nHospitalisations totales : ${datas.hospitalises}. (Nouvelles hospitalisations ce jour -> ${datas.nouvellesHospitalisations})\r\nRéanimations totales : ${datas.reanimation} (Nouvelles réanimations ce jour -> ${datas.nouvellesReanimations}).\r\nSource => ${datas.source.nom}. `,
         })
         .then((result) => {
           console.log('You successfully tweeted this : "' + result.text + '"');
         })
         .catch(console.error);
     });
-}, 86400);
+});
